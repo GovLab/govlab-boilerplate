bdddcc90e2fe9a649de3ccb911c6765afc809d99
Claudio Mendonca <claudioccm@gmail.com>
2016-08-25 11:00:31 -0400

    updated boilerplate outline

--- folders.py
+++ folders.py
@@ -1,15 +1,14 @@
-.gitignore
-# hopefully we will have public in ignore list
+.gitignore # we ignore public and any auto generated files for repository sanitation.
 
 package.json
-# npm package for styleguide PRECOMPILED SASS files
 # Styleguide Updates to be done via `npm update`
 
 source/
   vendor/
-    styleguide/   # styleguide precompiled SASS, JS and Image files
+    styleguide/   # styleguide SASS, JS and Image files | I want this as SASS files so we can use the variables, and @extend classes.
       images/**
       components/**
+      styleguide.scss
       foundation.min.css
       jquery-2.1.4.min.js
       slick.css
@@ -24,22 +23,48 @@ source/
     index.html    # index example. Will be tweaked for the project needs
                   # temp vinyl files whould not be saved here, or at least kept in .gitignore
   data/
-    global.yaml     # global data file, available anywhere in the system using {{global.variable}}
-    pages/        # any file.yaml in this folder, will be conpiled into a pages.yaml(json) file for generatePages()
-
+    data.json     # global data file, available anywhere in the system using {{global.variable}} | Check below
+    
   sass/           # site specific styles. Specific components, tweaks or new reusable components are added here.
-    styles.scss   # @import ../vendor/styleguide/* and _custom.scss
+    styles.scss   # @import ../vendor/styleguide/**.scss and _custom.scss
     _custom.scss
 
 gulpfile.js
-# simple tasks - copying files, minification, etc
-# imports 'extra-tasks.js'
+# imports 'extra-tasks.js' | or whichever name you prefer :)
+# Task - clean - clean public on watch
+# Task - browser-sync
+# Task - sass - with sourcemaps, and error log (not breaking gulp watch)
+# Tasks - vendor, image, js - copying files from source to public - maybe watch the data/ folder?
+# Task - nunjucks
+# Tasks - push-gh-master, push-gh-pages - these areused for the deploy task
+# Task - deploy
+# Task - watch
+# Task - default
 
 extra_tasks.js
-# long file with the big generic tasks, like generatePages(), concatenate multiple yaml files into one single file, etc.
+# long file with the long tasks
 # Initial Extra Tasks
-# 1- plug data.json into nunjucks tasks.
-# 2- generatePages() - cleaned version of OGRX. Generate html files from yaml items
-# 3- concatenateYaml() - generate one long yaml(JSON) file from multiple YAML files in a specific folder
+# - convert yaml to json
+# - data.json to aggregate data from local and remote sources (contentful thingy)
+# - generatePages() - cleaned version of OGRX. Generate html files from yaml item. 
+#   Ideally not creating any temp files inside source/ folder.
 
-# Markdown/Front-matter - will come in teh future
+
+data.json # Example
+{ 'global': [
+    { 
+      'posts': 'http://url-from-contentful-api.json',
+      'template': 'posts.html'
+    },{ 
+      'resources': 'http://url-from-contentful-api.json',
+      'template': ''
+    },{
+    ...
+    }
+]}
+
+
+
+
+
+
