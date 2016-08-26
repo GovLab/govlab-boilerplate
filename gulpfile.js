6d790ed5a3304726c476a2e95d205e0445ae45fc
mocxd <mark.adkins.hastings@gmail.com>
2016-08-25 21:18:53 -0400

    add http requests (nonworking)

--- gulpfile.js
+++ gulpfile.js
@@ -282,6 +282,33 @@ gulp.task('nunjucks', function () {
     .pipe(browserSync.reload({ stream: true }));
 });
 
+gulp.task('nunjucksHTTP', function() {
+  return gulp.src('source/templates/**/*.+(html|nunjucks)')
+  .pipe(data(function(file, cb) {
+
+    var httpData = '';
+    request
+    .get('https://raw.githubusercontent.com/GovLab/orgpedia-prototype/master/source/data/company.json')
+    .on('response', function (response) {
+      response.on('data', (chunk) => {
+        if (chunk) { httpData += chunk };
+      });
+      response.on('end', () => {
+        if (httpData.length) { console.log('Stream data recieved from HTTP (length)', httpData.length); }
+        else { console.log('No data received from HTTP'); }
+        cb(undefined, JSON.parse(httpData));
+      });
+    });
+
+  }))
+  .pipe(nunjucksRender({
+    path: ['source/templates'],
+    manageEnv: nunjucksEnv
+  }))
+  .pipe(gulp.dest('public'))
+  .pipe(browserSync.reload({ stream: true }));
+});
+
 gulp.task('push-gh-master', shell.task(['git push origin master']));
 
 gulp.task('push-gh-pages', function () {
